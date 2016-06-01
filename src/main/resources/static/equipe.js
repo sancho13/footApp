c4b8523073b1dc4d91a535beea80a1ec2c5aa2f1
Nabil Adouani <nabil.adouani@gmail.com>
2016-06-02 00:56:50 +0200

    Refactor EquipeService

--- src/main/resources/static/equipe.js
+++ src/main/resources/static/equipe.js
@@ -1,21 +1,47 @@
 /**
  * Created by Marseillais on 01/06/2016.
  */
-var app=angular.module('helloJS',[]);
+var app = angular.module('helloJS', []);
 
 
-app.controller('Hello',function ($scope, $http) {
+// Déclarer le service EquipeService pour centraliser les accès aux APIs
+app.factory('EquipeService', function ($http) {
 
-    $http.get('http://localhost:8080/api/equipes/1').
-    success(function(data) {
-        $scope.equipe = data;
-    });
+    // Une factory doit renvoyer un object qui contient les méthodes du service
+    return {
+
+        // La méthode qui récupère la liste des équipes
+        list: function () {
+            return $http.get("/api/equipes");
+        },
+
+        // La méthode qui récupère une équipe par son ID
+        get: function (equipeId) {
+            return $http.get("/api/equipes/" + equipeId);
+        }
+    };
 
 });
 
-app.factory('restservice', function ($http) {
-    $http.get(
-        "http://localhost:8080/api/equipes").success(function($scope,restservice)
-    {$scope.equipes = [restservice];});
+// Ce controlleur dépend de $scope et de EquipeService pour pouvoir appeler les méthodes list et get
+app.controller('Hello', function ($scope, EquipeService) {
+
+
+    // Get la liste des équipes
+    EquipeService.list().success(function (data) {
+
+        console.log('Retour de EquipeService.list():');
+        console.log(data);
+
+        $scope.equipes = data;
+    });
+
+    // Get l'équipe numéro 1
+    EquipeService.get(1).success(function (data) {
+        console.log('Retour de EquipeService.get(1):');
+        console.log(data);
+
+        $scope.equipe = data;
+    });
 
 });
